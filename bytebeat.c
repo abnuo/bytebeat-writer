695bcba31aad8fb22e940fa6eb03a256e32d6a5c
abnuo <66645994+abnuo@users.noreply.github.com>
2021-10-28 10:42:25 +0100

    Update bytebeat.c

--- bytebeat.c
+++ bytebeat.c
@@ -1,4 +1,4 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-char header[] = { 46, 115, 110, 100, 0, 0, 0, 32, 1, 72, 162, 24, 0, 0, 0, 2, 0, 0, 31, 64, 0, 0, 0, 1 }; int main(int argc, char *argv[], int t) {FILE *fp;int samples = atoi(argv[1]);fp = fopen("audio.au", "w");fwrite(header, sizeof(header), 1, fp);printf("Writing started\n");for (t=0; t<samples; t++) {printf("Writing...");putc(t&t>>8, fp);}printf("Done\n");}
+char header[] = { 46, 115, 110, 100, 0, 0, 0, 32, 1, 72, 162, 24, 0, 0, 0, 2, 0, 0, 31, 64, 0, 0, 0, 1 };int main(int argc, char *argv[], int t){FILE *fp;int samples = atoi(argv[1]);fp = fopen("audio.au", "w");fwrite(header, sizeof(header), 1, fp);printf("Writing started\n");for (t=0; t<samples; t++){printf("Writing...");putc(t&t>>8, fp);}printf("Done\n");}
